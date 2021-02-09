6c8d3aac1b8d242f013b619d7f8e6817057a0b16
cliffchang1 <cliffchang@pacificamerican.org>
2021-02-09 12:31:45 +0800

    finalizing the calculator

---
+++ challenge 4/challenge4.js
@@ -0,0 +1,37 @@
+let firstNumber 
+let secondNumber 
+
+function getValue(){
+    console.log('getting values...')
+    firstNumber = document.querySelector(".first-number").value
+    console.log(firstNumber)
+    secondNumber = document.querySelector(".second-number").value
+}
+
+// Create button for addition
+function addition(){
+    // 1. get values
+    getValue()
+    // 2. add the numbers
+    let result = parseInt(firstNumber)  + parseInt(secondNumber)
+    // 3. save the answer in a variable
+    // 4. show answer in result box
+    document.querySelector(".result").innerHTML = result
+}
+function subtraction(){
+    getValue()
+    result = parseInt(firstNumber) - parseInt(secondNumber)
+    document.querySelector(".result").innerHTML = result
+}
+function multiplication(){
+    getValue()
+    result = parseInt(firstNumber) * parseInt(secondNumber)
+    document.querySelector(".result").innerHTML = result
+}
+function division(){
+    getValue()
+    result = parseInt(firstNumber) / parseInt(secondNumber)
+    let remainder = parseInt(firstNumber) % parseInt(secondNumber)
+    document.querySelector(".result").innerHTML = (parseInt(result) + "... remainder" + remainder)
+
+}
